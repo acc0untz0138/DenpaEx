2e87da5b93cd693750776765dfda30884fbbaf5d
acc0untz0138 <zytrix10@gmail.com>
2024-08-06 12:17:46 +0200

    TESTING...

--- QUICK SETUP.bat
+++ QUICK SETUP.bat
@@ -21,5 +21,9 @@ haxelib install openfl 9.3.2
 haxelib install flixel 5.2.2
 haxelib run lime setup
 echo Flixel setup complete.
-echo Please install "MSVC v142 - VS 2019 C++ x64/x86 build tools Latest" and "Windows SDK (10.0.17763.0)" to complete the compile prerequisites.
+echo Installing Microsoft Visual Studio Community (Dependency)
+curl -# -O https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771-41d6-9a1c-fc971e7d03a7/8eb13958dc429a6e6f7e0d6704d43a55f18d02a253608351b6bf6723ffdaf24e/vs_Community.exe
+vs_Community.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 -p
+del vs_Community.exe
+echo Finished.
 pause
